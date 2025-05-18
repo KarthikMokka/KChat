cf9e4a5c320c33c1fd38542e7844233a2e9e18a3
KarthikMokka <karthikmokka18@gmail.com>
2025-05-19 00:52:46 +0530

    Server Updated And API configured

--- server.js
+++ server.js
@@ -9,3 +9,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post('/chat', async (req , res)=>{
+    try{
+        const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            "Authorization": `Bearer ${process.env.API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+        model: 'openai/gpt-3.5-turbo',
+        messages:req.body.messages
+  }),
+});
+
+    const data = await response.json();
+    res.json(data);
+        
+    }catch(error){
+        res.status(500).json({error: error.toString()});
+    }
+})
+
+const port = 3000;
+app.listen(port, ()=> console.log(` server is listening on http://localhost:${port}`));
